b8ff789529269a197fc6da276ef942ecad6efb01
edu-hc <edu.carvc@gmail.com>
2024-08-14 22:53:00 -0300

    Upload 3 projetos

---
+++ projeto Concessionária/src/projetoaula2/Automovel.java
@@ -0,0 +1,18 @@
+package projetoaula2;
+
+public class Automovel {
+	
+		String marca;
+		String modelo;
+		String cor;
+		double preco;
+	
+	public void Informa_Automovel() {
+		
+		System.out.println("MARCA: " + marca);
+		System.out.println("MODELO: " + modelo);
+		System.out.println("COR: " + cor);
+		System.out.println("PRECO: " + preco);
+	}
+
+}
---
+++ projeto Universidade/src/projetoaula/fatec.java
@@ -0,0 +1,19 @@
+package projetoaula;
+
+public class fatec {
+	
+	//atributos
+	String unidade;
+	String curso;
+	
+	//métodos
+	public void Retorna_unidade(String u) {
+		System.out.println("Unidade da Fatec:\t" + u);
+	
+	}
+	
+	public void Retorna_curso(String c) {
+		System.out.println("Curso da Fatec:\t" + c);
+	}
+
+}
